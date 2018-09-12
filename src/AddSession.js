603e0d271dbf9a1955a3c50344ed23becf98fbbb
henry montoya <jhenrymontoya@gmail.com>
2018-09-12 10:09:48 -0700

    wip

--- src/AddSession.js
+++ src/AddSession.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 class AddSession extends React.Component {
-  state = {};
+  state = {
+    gameId: 0,
+    gameTypeId: 0,
+    stakeId: 0,
+    limitTypeId: 0,
+    locationId: 0,
+    locationTypeId: 0,
+    buyIn: 0,
+    cashedOut: 0,
+    startTime: "",
+    endTime: "",
+    breakDuration: "",
+    notes: []
+  };
 
   render() {
-    return <div />;
+    return (
+      <div className="container">
+        <div className="card">
+          <div className="card-heading">
+            <h1>Add Session</h1>
+          </div>
+          <div className="card">
+            <select name="game" />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
-
 export function postNewUser(payload) {
-  return axios.post("/api/users/", payload);
+  return axios.post("/api/users", payload);
 }
 
 export function removeUser(id) {
